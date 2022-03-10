7931bedf176eeb035f7d85b17c9cd7c68c98473b
Dominic Misasi <meshtatsuo@gmail.com>
2022-03-09 19:49:17 -0700

    finished up api routes and backend management

--- routes/apiRoutes/api.js
+++ routes/apiRoutes/api.js
@@ -1,11 +1,30 @@
 const path = require("path");
 const router = require("express").Router();
 
+// note management library
+const { addNewNote, removeNoteById } = require("../../lib/notes");
+
 const db = require("../../db/db.json");
 
-router.get("/api", (req, res) => {
-  console.log(db);
-  res.send(db);
+router.get("", (req, res) => {});
+
+router.get("/notes", (req, res) => {
+  //return note db json
+  res.json(db);
 });
 
+router.post("/notes", (req, res) => {
+  if (addNewNote(req.body)) {
+    res.send("Success");
+  } else {
+    res.send("Error");
+  }
+});
+
+router.delete("/notes/:id", (req, res) => {
+  console.log(req.params);
+  if (removeNoteById(req.params.id)) {
+    res.send("Success");
+  }
+});
 module.exports = router;
